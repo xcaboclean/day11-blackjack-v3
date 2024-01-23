119be2c84a7106ab1fdb6ecefc833d613d747465
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2024-01-23 01:47:22 +0000

    new feat - function create_deck()

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ from replit import clear
 from art import logo
 
 
+def create_deck():
+suits = ["♠️", "♣️", "♥️", "♦️", "♠️", "♣️", "♥️", "♦️"]
+values_cards = [
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+    'A',
+]
+return [{
+    'value_card': value_card,
+    'suit': suit
+} for value_card, suit in itertools.product(values_cards, suits)]
+
 
 
 
